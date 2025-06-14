b88fd84f0a00bf88d16591671943a3fd9d8f2e53
Abu Bakr Iznain <168574041+abubakriz@users.noreply.github.com>
2025-06-14 13:31:43 +0200

    Add DOMPurify

--- src/features/productDetails/ui/ProductDescription.tsx
+++ src/features/productDetails/ui/ProductDescription.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import { IProduct } from "@/features/productDetails/models/ProductDetails";
 import { Separator } from "@/shared/components/Separator";
+import DOMPurify from "dompurify";
 
 export const ProductDescription = ({ product }: { product: IProduct }) => {
   return (
     <section className="space-y-4" aria-labelledby="description">
       <h2 id="description">تفاصيل المنتج</h2>
       <Separator />
-      <div dangerouslySetInnerHTML={{ __html: product.Description }}></div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: DOMPurify.sanitize(product.Description),
+        }}
+      ></div>
     </section>
   );
 };
